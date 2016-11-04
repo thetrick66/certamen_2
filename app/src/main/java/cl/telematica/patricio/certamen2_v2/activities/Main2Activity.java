5364a788e943037e3c940ae0dd2343589e1ef41d
Patricio <patricio.rodriguezg@alumnos.usm.cl>
2016-11-04 17:51:05 -0300

    Tarea 1 Patricio Rodriguez 201130007-6

--- app/src/main/java/cl/telematica/patricio/certamen2_v2/activities/Main2Activity.java
+++ app/src/main/java/cl/telematica/patricio/certamen2_v2/activities/Main2Activity.java
@@ -88,6 +88,7 @@ public class Main2Activity extends AppCompatActivity implements Main2View, ItemC
         task.execute();
 
     }
+    
     @Override
     public void onClick(View view, int position) {
         final repos repo = reposes.get(position);
